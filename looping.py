a3847f23e58a66860a2bb34f3bc65f2db6ad6ea2
Samruddhi Rasal <samruddhi.rasal03@gmail.com>
2026-09-11 23:57:03 +0530

    loops

---
+++ looping.py
@@ -0,0 +1,20 @@
+marks = 45
+if marks >= 90:
+    print("Grade A+")
+    if marks >= 95:
+        print("Outstanding")
+        print("Keep it up! Great work")
+elif marks >= 80:
+    print("Grade A")
+elif marks >= 70:
+    print("Grade B")
+elif marks >= 60:
+    print("Grade C")
+else:
+    print("Grade D")
+'''
+day = 3
+match day:
+    case 1:
+        print("Monday")
+        '''
