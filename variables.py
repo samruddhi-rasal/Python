bbb7170ff00a0b534ed2979929a755329de0a4c8
Samruddhi Rasal <samruddhi.rasal03@gmail.com>
2026-09-11 11:40:39 +0530

    Variables

--- variables.py
+++ variables.py
@@ -1,5 +1,5 @@
 #Integer
-age = 52
+age = 22
 
 #String
 name = "Samruddhi Rasal"
@@ -10,3 +10,36 @@ salary= 500000.50
 #Boolean 
 is_intern = True 
 
+#Another Integer
+experience = 2
+print("Hello, World!")
+
+print("Age:", age)
+print("Name:", name)
+print("Salary:", salary)
+print("Is Intern:", is_intern)
+print("Experience:", experience)
+
+#Manipulating Variables
+age = 22
+next_year_age = age + 1
+
+print("Current age:", age)
+print("Next year age:", next_year_age)
+
+#Combining Variables
+first_name = "Samruddhi"
+last_name = "Rasal"
+
+full_name = first_name + " " + last_name
+
+print("Full Name:", full_name)
+
+
+#String Interpolation
+name = "Samruddhi"
+age = 22
+print(f"My name is {name} and my age {age}")
+
+age = 22
+print(f"Next year I will be {age + 1} years old.")
