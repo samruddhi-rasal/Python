6b44215d793db7f7f0db398980d8863440cb9c2b
Samruddhi Rasal <samruddhi.rasal03@gmail.com>
2026-09-11 13:17:05 +0530

    Comments

---
+++ comments.py
@@ -0,0 +1,22 @@
+# This program shows a program's components
+# Defination of function SeeYou
+
+"""
+Multiline Comments in Python
+"""
+
+def SeeYou():
+    print("Time to say Good Bye!")
+
+count = 56 
+productCount=count+56
+print(count)
+print(type(count))
+print(productCount)
+if(productCount > count):
+    print("Product count is good")
+else:
+    print("Product is low")
+
+SeeYou();
+   
