904a4feb20cb7a5837e4d8d6651674b36201dc6d
Samruddhi Rasal <samruddhi.rasal03@gmail.com>
2026-09-12 23:34:05 +0530

    loops

--- looping.py
+++ looping.py
@@ -12,9 +12,26 @@ elif marks >= 60:
     print("Grade C")
 else:
     print("Grade D")
-'''
+
+#switch case 
 day = 3
 match day:
     case 1:
         print("Monday")
-        '''
+    case 2:
+            print("Tuesday")
+    case 3:
+            print("Wednesday")
+    case 4:
+            print("Thrusday")
+    case 5:
+            print("Friday")
+    case 6:
+            print("Saturday")
+    case 7:
+            print("Sunday")
+    case _:
+            print("Invalid day")
+
+
+
