c62d19fa976ebd9c931609b4797d13437d372d91
Samruddhi Rasal <samruddhi.rasal03@gmail.com>
2026-09-13 23:45:27 +0530

    Collections

---
+++ collectionsdemo.py
@@ -0,0 +1,24 @@
+# Lists (mutable)
+
+names=["Ravi", "Sachin", "Manoj", "Aarush"]
+marks=[76,78,45,76,89,98]
+print(names)
+print(marks)
+marks[0]=99
+names[1]="Rahul"
+print(names)
+print(marks)
+
+#Tuples (immutable)
+
+p=(54,65,34,76,23)
+q=(99,67,87,45)
+r=('a', 'b','o')
+
+
+# Sets
+myset={76,78,9,45}
+
+#Dictionary
+vowels= { 'a':1, 'e':2, 'o':4, 'u':5}
+
