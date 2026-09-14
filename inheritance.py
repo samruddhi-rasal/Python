eaf1f872fefff747bc61924c9ddfcf25b0f77ce5
Samruddhi Rasal <samruddhi.rasal03@gmail.com>
2026-09-14 23:27:42 +0530

    Inheriatnce

---
+++ inheritance.py
@@ -0,0 +1,28 @@
+class Person():  
+        
+    # Constructor  
+    def __init__(self, name):  
+        self.name = name  
+    
+    # To get name  
+    def getName(self):  
+        return self.name  
+    
+    # To check if this person is employee  
+    def isEmployee(self):  
+        return False
+    
+    
+# Inherited or Sub class (Note Person in bracket)  
+class Employee(Person):  
+    
+    # Here we return true  
+    def isEmployee(self):  
+        return True
+    
+ 
+emp = Person("Ganesh Shinde")  # An Object of Person  
+print(emp.getName(), emp.isEmployee())  
+    
+emp = Employee("Kishan Gupta") # An Object of Employee  
+print(emp.getName(), emp.isEmployee()) 
